112a35bb812356b6575cb459843975c38f10726f
Dan <danielshan824@gmail.com>
2026-01-17 17:38:35 -0500

    Edits for VSC
    Made code compatible with VSC (hopefully will still work with CLion)

---
+++ build/CMakeFiles/hello_vitals.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_vitals.dir/hello_vitals.cpp.o"
+  "CMakeFiles/hello_vitals.dir/hello_vitals.cpp.o.d"
+  "hello_vitals"
+  "hello_vitals.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_vitals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
